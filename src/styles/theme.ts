439682e1a989582b7c3f6a98fc998cbd13e738a4
park_so_young <soyoung931014@gmail.com>
2022-09-20 02:57:45 +0900

    feat: color 추가

--- src/styles/theme.ts
+++ src/styles/theme.ts
@@ -1,14 +1,19 @@
 const color = {
   grey_01: '#6A697C',
+  grey_02: '#C6D1DE',
+  grey_03: '#B8BDC7',
   purple_01: '#A69EFC',
   purple_02: '#9886F0',
   purple_03: '#9885EF',
   pink_01: '#E3ADC3',
   pink_02: '#FF375C',
 };
-
+const deviceSizes = {
+  mobile: '480px',
+};
 const theme = {
   color,
+  deviceSizes,
 };
 
 export default theme;
