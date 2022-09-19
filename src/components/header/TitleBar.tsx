7bf6df4699a1c4ee606384654853f506fc7c4917
park_so_young <soyoung931014@gmail.com>
2022-09-20 03:14:12 +0900

    feat: 레이아웃"

---
+++ src/components/header/TitleBar.tsx
@@ -0,0 +1,37 @@
+import React from 'react';
+import { FiX } from 'react-icons/fi';
+import styled from 'styled-components';
+const TitleBar = () => {
+  return (
+    <Container>
+      <IconWrapper>
+        <Closed />
+      </IconWrapper>
+      <TitleWrapper>
+        <Customer>곽지우</Customer>
+        <Day>날짜</Day>
+      </TitleWrapper>
+    </Container>
+  );
+};
+
+export default TitleBar;
+
+const Container = styled.div`
+  display: flex;
+  align-items: center;
+`;
+const IconWrapper = styled.div``;
+const TitleWrapper = styled.div`
+  margin-left: 180px;
+`;
+
+const Closed = styled(FiX)`
+  width: 50px;
+  height: 70px;
+  color: ${({ theme }) => theme.color.grey_02};
+`;
+const Customer = styled.div`
+  width: 400px;
+`;
+const Day = styled.div``;
