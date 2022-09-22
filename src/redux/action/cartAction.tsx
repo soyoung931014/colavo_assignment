5a7492b243830ea9c155d5e6eb64cbaac150ad97
park_so_young <soyoung931014@gmail.com>
2022-09-22 23:20:59 +0900

    feat: 장바구니 추가 상태관리(#21)

--- src/redux/action/cartAction.tsx
+++ src/redux/action/cartAction.tsx
@@ -1,9 +1,16 @@
 export const FETCH_CART_INFO = 'FETCH_CART_INFO';
+export const SAVE_CART = 'SAVE_CART';
 export const DELETE_CART = 'DELETE_CART';
 
 export const fetchCartInfo = data => {
   return {
     type: 'FETCH_CART_INFO',
+    payload: data,
+  };
+};
+export const saveCart = data => {
+  return {
+    type: 'SAVE_CART',
     payload: data,
   };
 };
