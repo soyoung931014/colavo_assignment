1af273d74005a8ab3f1a40c2d893e0612d8d8a22
park_so_young <soyoung931014@gmail.com>
2023-02-23 16:55:32 +0900

    fix: 리듀서 로직수정

--- src/redux/reducers/discountReducer.tsx
+++ src/redux/reducers/discountReducer.tsx
@@ -5,10 +5,10 @@ const initialState: AddCheckDiscount[] = [];
 
 const discountReducer = (state = initialState, action) => {
   switch (action.type) {
-    case FETCH_DISCOUNT_INFO:
+    case FETCH_DISCOUNT_INFO: // 장바구니 불러오는 액션인데 필요한가 싶다.
       return [...state, ...action.payload];
-    case APPLY_DISCOUNT:
-      return [...action.payload];
+    case APPLY_DISCOUNT: // 장바구니 담는 액션
+      return [...state, ...action.payload];
     default:
       return state;
   }
