77c81d52d005d92141b9e42768290ec565986c6b
park_so_young <soyoung931014@gmail.com>
2023-03-22 17:00:33 +0900

    chore:jest 절대경로 생성

---
+++ jest.config.js
@@ -0,0 +1,17 @@
+module.exports = {
+  testPathIgnorePatterns: ['<rootDir>/node_modules/'],
+  transform: {
+    '^.+\\.(js|jsx|ts|tsx)$': 'ts-jest',
+  },
+  moduleNameMapper: {
+    '^@src/(.*)$': '<rootDir>/src/$1',
+    '^@routes/(.*)$': '<rootDir>/src/routes/$1',
+    '^@api/(.*)$': '<rootDir>/src/api/$1',
+    '^@components/(.*)$': '<rootDir>/src/components/$1',
+    '^@pages/(.*)$': '<rootDir>/src/pages/$1',
+    '^@styles/(.*)$': '<rootDir>/src/styles/$1',
+    '^@type/(.*)$': '<rootDir>/src/type/$1',
+    '^@hooks/(.*)$': '<rootDir>/src/hooks/$1',
+  },
+  testEnvironment: 'jest-environment-jsdom',
+};
