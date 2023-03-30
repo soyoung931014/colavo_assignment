a24340368f8f43cbf5e9907ebd6bf9806d662ac2
park_so_young <soyoung931014@gmail.com>
2023-03-30 22:17:23 +0900

    test: item의 수량 선택 가능

--- src/test/Checkout.test.js
+++ src/test/Checkout.test.js
@@ -1,9 +1,19 @@
+import Count from '@src/components/modal/Count';
 import Checkout from '@src/pages/Checkout';
 import userEvent from '@testing-library/user-event';
 import { renderWithProviders, screen, act } from './utils';
 
 const event = userEvent.setup();
-describe('item, discount는 각각 장바구니로 추가/ 삭제 가능', () => {
+
+const setup = async () => {
+  const utils = renderWithProviders(<Count />);
+  const input = await screen.findByLabelText('count-input');
+  return {
+    input,
+    ...utils,
+  };
+};
+describe('item, discount는 각각 장바구니로 추가/삭제하기', () => {
   test('item 추가/삭제하기', async () => {
     renderWithProviders(<Checkout />);
     const hairMenu = screen.getByTestId('item');
@@ -76,4 +86,45 @@ describe('item, discount는 각각 장바구니로 추가/ 삭제 가능', () =>
     });
     expect(disItem).not.toBeInTheDocument();
   });
+});
+
+test('item의 수량 선택 가능 ex) 기본펌 X 3', async () => {
+  renderWithProviders(<Checkout />);
+  const hairMenu = screen.getByTestId('item');
+  await act(async () => {
+    await event.click(hairMenu);
+  });
+
+  const perm = await screen.findByText('기본펌');
+  await act(async () => {
+    await event.click(perm);
+  });
+
+  const completeButton = await screen.findByText('완료');
+  await act(async () => {
+    await event.click(completeButton);
+  });
+
+  const item = await screen.findByText('기본펌');
+  const price = await screen.findByTestId('price');
+  expect(item).toBeInTheDocument();
+  expect(price).toHaveTextContent('100,000');
+
+  const modalButton = screen.getByTestId('modal');
+  await act(async () => await event.click(modalButton));
+
+  const countInput = await screen.findByRole('textbox');
+  expect(countInput).toBeInTheDocument();
+
+  await act(async () => {
+    await event.type(countInput, '3');
+  });
+
+  const saveButton = await screen.findByRole('button', { name: '확인' });
+  await act(async () => {
+    await event.click(saveButton);
+  });
+  const count = await screen.findByTestId('count');
+  expect(count).toHaveTextContent('3');
+  expect(price).toHaveTextContent('300,000');
 });
